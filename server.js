9c01c119d86b73a64d88724a837042a26d3fee9a
amit karmacharya <amitkarmacharya@MacBook-Pro.local>
2020-12-27 17:50:22 -0500

    mongoose.connect to the server.js

--- server.js
+++ server.js
@@ -12,9 +12,16 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
 // routes
-app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}: http://localhost:${PORT}`);
