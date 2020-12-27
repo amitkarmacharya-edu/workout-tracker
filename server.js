cd393253b0b8c9f1a414908554955bbbbffb247d
amit karmacharya <amitkarmacharya@MacBook-Pro.local>
2020-12-27 16:47:17 -0500

    created html routes using express router

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}: http://localhost:${PORT}`);
